d45e462baa981215afd3e0f7db3f4ad733a62dfb
fancydev0225 <fancydev0225@gmail.com>
2022-04-03 14:04:12 -0700

    fancy completed demo landing page

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,6 +1,6 @@
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
+// import { Banner } from './Banner';
 import { Footer } from './Footer';
 import { Hero } from './Hero';
 import { VerticalFeatures } from './VerticalFeatures';
